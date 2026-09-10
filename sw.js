9915c83300c22484066bf6dfba2fcb28b032a0c6
gregorbuerger <gregor.buerger@googlemail.com>
2026-09-10 04:27:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wildoku-beta-v0.7.0';
+const CACHE='wildoku-beta-v0.7.1';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
